fdee15a18cecfc9251b777863fd447b1820d3ee1
Tuân <nguyenvantuanvp1999@gmail.com>
2025-03-11 09:11:10 +0700

    add bai6

---
+++ demo.js
@@ -0,0 +1,8 @@
+console.log("hello World! Van Tuan");
+document.write("Hello World! Van Tuan");
+document.getElementById("demo").innerHTML
+    = "<h3>Khai giang vao dau nam</h3>";
+document.getElementById("demo2").innerHTML
+= "<h3>Khai giang vao dau nam</h3>";
+name =prompt("moi ban nhap ten");
+document.getElementById("demo1").innerText = name
